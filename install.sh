7805c5d0f8a9dc4227de7433db85109fcee88d80
Chris Brenton <chrisb@ifixit.com>
2012-02-20 11:10:37 -0800

    Added install script.

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mv $HOME/.vimrc $HOME/.vimrc.old
+ln -s ./vimrc $HOME/.vimrc
+mv $HOME/.vim $HOME/.vim.old
+ln -s ./vim $HOME/.vim
+mv $HOME/.zshrc $HOME/.zshrc.old
+ln -s ./zshrc $HOME/.zshrc
+mv $HOME/.gitconfig $HOME/.gitconfig.old
+ln -s ./gitconfig $HOME/.gitconfig
+if [ -d $HOME/.oh-my-zsh ]
+then
+   ln -s ./mine.zsh-theme $HOME/.oh-my-zsh/themes/mine.zsh-theme
+else
+   echo "Could not install mine.zsh-theme. Try installing oh-my-zsh if it isn't already installed."
+fi
